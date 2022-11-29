b191d2fbfc92003343d1ff4298bc17f1f13e2825
Burak Caniklioğlu <burakcaniklioglu@gmail.com>
2022-11-29 19:12:38 +0300

    darkMode add

--- src/pages/Favorite/index.js
+++ src/pages/Favorite/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Favorites from '../../components/Favorites';
 import Navbar from '../../components/Navbar';
 import './favorite.scss';
 
 function FavoritePage() {
+  const theme = useSelector((state) => state);
+  const { darkMode } = theme;
   return (
     <>
       <Navbar />
-      <main className="favorite-section">
+      <main className="favorite-section" style={{backgroundColor: darkMode && '#171801'}} >
         <section className="favorite-container">
           <Favorites />
         </section>
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/cartSlice';
 import favoriteReducer from '../features/favoriteSlice';
+import themeReducer from '../features/themeSlice';
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
     favorites: favoriteReducer,
+    darkMode:themeReducer,
   },
 });
